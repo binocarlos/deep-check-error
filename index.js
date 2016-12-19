8ab9422943929848f19d1a51755497b9a40969cf
Kai Davenport <kaiyadavenport@gmail.com>
2016-12-19 14:16:38 +0000

    passing test

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var deepCheck = require('deep-check')
 
 module.exports = function(data, schema, message){
-  
+  message = message || 'required settings are missing: '
+  var missing = deepCheck(data, schema) || []
+  if(missing.length<=0) return
+  throw new Error(message + missing.join(', '))
 }
